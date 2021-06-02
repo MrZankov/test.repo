8ba880098ac74ee64aa49aa745bba4610654ec93
ezankov <ezankov@gmail.com>
2021-06-02 11:16:50 +0930

    Update API

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template
 app = Flask('app')
+import requests
+import json
+
+api_key = "f77b8ad7c7df72c5a9169681620e91ce"
+lat = "48.208176"
+lon = "16.373819"
+url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (lat, lon, api_key)
+response = requests.get(url)
+data = json.loads(response.text)
+print(data)
+
 
 @app.route('/')
-def hello_world():
-  return render_template('index.html')
-
+def index():
+  return render_template('index.html', data=data)
 
 app.run(host='0.0.0.0', port=8080)
